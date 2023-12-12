a183dc75dbaae0ae3e447444534406911ca583a7
thanhnx12 <thanhnx1209@gmail.com>
2023-12-12 21:19:08 +0700

    update deploy model

--- deploy_model/readme.md
+++ deploy_model/readme.md
@@ -1,24 +1,21 @@
-# 1.Zip the contents of this directory as pytorch_model.bin
-```sh
-zip -r model/pytorch_model.bin .
-```
-# 2. Install torch-model-archiver .
+
+# 1. Install torch-model-archiver .
 ```sh
 pip install torch-model-archiver
 ```
-# 3.Create .mar file by running below command on terminal.
+# 2.Create .mar file by running below command on terminal.
 ```sh
-torch-model-archiver --model-name sentence_Transformer_BERT --version 1.0 --serialized-file pytorch_model.bin --handler run_handler.py --extra-files handler.py --export-path <output-path> --runtime python3 -f
+torch-model-archiver --model-name sbert --version 1.0 --serialized-file model/pytorch_model.bin --handler run_handler.py --extra-files "model/config.json,model/vocab.txt" --export-path .
 ```
 
-# 4. Create a dockerfile packaging all the contents require to serve the model and start the server.
+# 3. Create a dockerfile packaging all the contents require to serve the model and start the server.
 
-# 5. Build docker image
+# 4. Build docker image
 ```sh
 docker build -t ptserve-sbert:v1 dockerfile .
 ```
 
-# 6. Run the container locally
+# 5. Run the container locally
 ```sh
 docker run --rm -it -p 3000:8080 ptserve-sbert:v1
 ```
--- deploy_model/run_handler.py
+++ deploy_model/run_handler.py
@@ -1,4 +1,54 @@
-from handler import SentenceTransformerHandler
+#from handler import SentenceTransformerHandler
+from sentence_transformers import SentenceTransformer
+import json
+import zipfile
+from json import JSONEncoder
+import numpy as np
+import os
+
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+    
+    
+class SentenceTransformerHandler(object):
+    def __init__(self):
+        super(SentenceTransformerHandler, self).__init__()
+        self.initialized = False
+        self.embedder = None
+    def initialize(self, context):
+        properties = context.system_properties
+        model_dir = properties.get("model_dir")
+        
+        try:
+            with zipfile.ZipFile(model_dir + '/pytorch_model.bin',  'r') as zip_ref:
+                zip_ref.extractall(model_dir)
+        except:
+            print('tried unzipping again')
+        self.embedder = SentenceTransformer(model_dir)
+        self.initialized = True
+    def preprocess(self, data):
+        
+        inputs = data[0].get("data")
+       
+        if inputs is None:
+            inputs = data[0].get("body")
+        inputs = inputs.decode('utf-8')
+        inputs = json.loads(inputs)
+        sentences= inputs['queries']
+        return sentences
+    def inference(self, data):
+        query_embeddings = self.embedder.encode(data)
+        return query_embeddings
+    def postprocess(self, data):
+        return [json.dumps(data,cls=NumpyArrayEncoder)]
+
+
+
+
 _service = SentenceTransformerHandler()
 def handle(data, context):
     """
